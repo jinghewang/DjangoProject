5f2459bdad5b7929c2972b3acdfcbe46e829554e
jinghewang <jinghewang@163.com>
2015-09-11 23:40:40 +0800

    models search_fields

--- student/admin.py
+++ student/admin.py
@@ -28,6 +28,7 @@ class QuestionAdmin(admin.ModelAdmin):
     # fields = ['pub_date','question_text']
     list_display = ('question_text', 'pub_date','was_published_recently')
     list_filter = ['pub_date']
+    search_fields = ['question_text']
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'],'classes': ['collapse']}),
